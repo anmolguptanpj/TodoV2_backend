e78db41478739be6f486dd1fe1271144deb1ce17
anmolguptanpj <anmolguptanpj282@gmail.com>
2025-11-05 15:02:50 +0530

    maintaining todo and user link data model with authorization system

---
+++ src/controllers/user_controller.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+
+const userSchema =  new mongoose.Schema({
+    firstName:{
+        type:String,
+        unique:false,
+        required:true,
+        trim:true
+    },
+    lastName:{
+        type:String,
+        unique:false,
+        required:true,
+        trim:true
+    },
+    username:{
+        type:String,
+        unique:true,
+        trim:true,
+        lowercase:true,
+        required:true,
+    },
+    email:{
+        type:String,
+        unique:true,
+        trim:true,
+        lowercase:true,
+        required:true,
+    },
+    password:{
+        type:String,
+        trim:true,
+        required:true
+    }
+})
